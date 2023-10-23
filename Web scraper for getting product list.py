c28ebb8aae5a08a6f3242ce07e3502a823d672c0
Mariusz Glapiak <145092022+Kempo87@users.noreply.github.com>
2023-10-23 05:57:01 +0200

    Add files via upload

---
+++ Web scraper for getting product list.py
@@ -0,0 +1,23 @@
+import click
+from lxml import html 
+import requests 
+
+def extract(page_content, xpath):
+    tree = html.fromstring(page_content)
+    elements = tree.xpath(xpath)
+    elements = [e.text_content() for e in elements]
+    stripped = [e.strip().replace('\n', ' ') for e in elements]
+    return stripped
+
+@click.command()
+@click.argument('url')
+@click.argument('xpath')
+def main(url, xpath):
+    page = requests.get(url)
+    content = page.text
+    elements = extract(content, xpath)
+    for element in elements:
+        print(element)
+
+if __name__ == "__main__":
+    main()
